49ed6763120d9001038a99ef5d84a93547b98737
gizmore <gizmore@wechall.net>
2022-10-07 07:01:34 +0200

    '7.0.1-r1649 German Lang Pack - I'

---
+++ lang/cc_de.php
@@ -0,0 +1,4 @@
+<?php
+namespace GDO\CountryCoordinates\lang;
+return [
+'mt_countrycoordinates_detect' => 'Land erkennen','t_detected_country' => 'Ländererkennung','p_detected_country' => 'Als Ergebnis wurde %s gewählt.','autodetect' => 'Automatische Erkennung bei Benutzeraktivierung?',];
