eaa5cdc76476ef8107d23e2bd512977cfc6357c0
gizmore <gizmore@wechall.net>
2022-10-07 04:35:24 +0200

    'Privacy and SSL'

--- Module_CountryCoordinates.php
+++ Module_CountryCoordinates.php
@@ -2,6 +2,8 @@
 namespace GDO\CountryCoordinates;
 
 use GDO\Core\GDO_Module;
+use GDO\UI\GDT_Divider;
+use GDO\Core\GDT_Checkbox;
 
 /**
  * Detect countries via lat/lng coordinates.
@@ -15,11 +17,11 @@ final class Module_CountryCoordinates extends GDO_Module
 	public int $priority = 250;
 	public string $license = 'ODbL';
 	
-	public function defaultEnabled() : bool { return false; }
+// 	public function defaultEnabled() : bool { return false; }
 	
 	public function onInstall() : void { InstallGeocountries::install(); }
 
-	public function thirdPartyFolders() : array { return ['/countries/']; }
+	public function thirdPartyFolders() : array { return ['/countries']; }
 	
 	public function onLoadLanguage() : void { $this->loadLanguage('lang/cc'); }
 	
@@ -37,5 +39,22 @@ final class Module_CountryCoordinates extends GDO_Module
 			'Maps',
 		];
 	}
+	
+	public function getPrivacyRelatedFields(): array
+	{
+		return [
+			GDT_Divider::make()->label('info_privacy_related_module', [$this->gdoHumanName()]),
+			$this->getConfigColumn('autodetect'),
+		];
+	}
 
+	public function getConfig(): array
+	{
+		return [
+			GDT_Checkbox::make('autodetect')->initial('0'),
+		];
+	}
+	public function cfgAutodetect(): bool { return $this->getConfigValue('autodetect'); }
+	
+	
 }
--- lang/cc_en.php
+++ lang/cc_en.php
@@ -4,5 +4,5 @@ return [
 	'mt_countrycoordinates_detect' => 'Detect Country',
 	't_detected_country' => 'Country Detection',
 	'p_detected_country' => 'As a result, %s got detected.',
-	
+	'autodetect' => 'Autodetect on User Activation?',
 ];
